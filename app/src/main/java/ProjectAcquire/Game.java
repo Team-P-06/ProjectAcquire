84d522042ef342e3578dbcf4298f3663d59655fa
Tyler Kelley <Kelltyl2@isu.edu>
2021-11-03 14:13:58 -0600

    Tyler's Classes

---
+++ app/src/main/java/ProjectAcquire/Game.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+package ProjectAcquire;
+
+/**
+ * Game class that can get the turns, start, load, or end a game
+ */
+public class Game {
+    GameState GameState = new GameState();
+
+    /**
+     * Load game method that accepts a gson file to get the game state of a specific game
+     * @param game
+     */
+    public void loadGame(String game){ }
+
+    /**
+     * Turn method that will determine which players turn it is
+     * @return
+     */
+    public GameState getTurn(){
+        return GameState;
+    }
+
+    /**
+     * Start game method that will begin a new game that isn't already saved
+     */
+    public void startGame(){ }
+
+    /**
+     * Run a game that is already saved from the gson file gathered from loadGame
+     * @param currentGame
+     */
+    public void runGame(GameState currentGame){ }
+
+    /**
+     * Once a player ends the game on their turn the game will end using this method and determine the winner
+     */
+    public void endGame(){ }
+}
---
+++ app/src/main/java/ProjectAcquire/GameState.java
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+package ProjectAcquire;
+
+/**
+ * GameSate class that contains a games current status
+ */
+public class GameState {
+    /**
+     * Variables needed to maintain a gamestate
+     */
+    Player currentPlayer;
+    Board currentBoard;
+
+    /**
+     * Determines which player has the next turn available in the gamestate
+     * @return
+     */
+    public Player nextTurn(){
+        return currentPlayer;
+    }
+
+    /**
+     * If a player has a turn let the player play their turn
+     * @param Player
+     * @return
+     */
+    public boolean hasTurn(Player Player){
+        return true;
+    }
+
+    /**
+     * If a player has a tile in their hand they can lay on the board let the player place the tile
+     * @param availableTile
+     * @return
+     */
+    public boolean hasTileToPlay(Tile availableTile){
+        return true;
+    }
+
+    /**
+     * Recursive play method that is called when a player decides to play their turn
+     */
+    public void playTurn(){}
+}
